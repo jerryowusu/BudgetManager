b1c59540242eeeb6a6a0eefddbe280e1fd1ed59b
Jerry Owusu <jerryowusu6133@gmail.com>
2022-07-01 12:41:30 -0400

    Update routes and location of welcome index

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,4 @@
-# frozen_string_literal: true
-
 class User < ApplicationRecord
-  has_many :categories, dependent: :destroy
-  has_many :payments, dependent: :destroy
-
-  validates :name, presence: true
-  endclass User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
