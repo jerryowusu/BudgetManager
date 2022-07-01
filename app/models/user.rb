e266f1a6d2cd6f07ea52d6a54e793da3f5ef81b5
Jerry Owusu <jerryowusu6133@gmail.com>
2022-07-01 12:05:31 -0400

    Create models

---
+++ app/models/user.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+class User < ApplicationRecord
+  has_many :categories, dependent: :destroy
+  has_many :payments, dependent: :destroy
+
+  validates :name, presence: true
+  endclass User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_many :categories, dependent: :destroy
+  has_many :payments, dependent: :destroy
+
+  validates :name, presence: true
+
+  def most_recent_categories
+    categories.order(created_at: :desc).limit(3)
+  end
+
+  def most_ancient_categories
+    categories.order(created_at: :asc).limit(3)
+  end
+end
